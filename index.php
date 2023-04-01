0a59eba7afb9ead88226d916dff3c72e0df93d59
GabroTestAccount <KabreelNohme2017@gmail.com>
2023-04-01 18:14:50 +0200

    layout page

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
-<?php 
+<?php
 include_once('connect.php');
+include_once('layout/header.php');
 ?>
 
+<h1>Index page</h1>
+<table class="table">
+    <thead class="table-light">
+        <div class="d-flex justify-content-center">
+            <h3>Students</h3>
+        </div>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Age</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="col">1</th>
+            <th scope="col">gabriel</th>
+            <th scope="col">nohme</th>
+            <th scope="col">23</th>
+        </tr>
+    </tbody>
+</table>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    <h1>Index page</h1>
-    </body>
-</html>
+<?php include_once('layout/header.php') ;?>
---
+++ layout/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <section>
+    <div class="container">
